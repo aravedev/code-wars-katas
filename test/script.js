cf302bc35eb437792a9c78cd40de14985431282f
aravedev <dravedev@gmail.com>
2022-07-07 11:25:23 +0200

    solving a kata that eliminate all vowels from a string

--- test/script.js
+++ test/script.js
@@ -1396,3 +1396,40 @@ describe("Basic tests", () => {
 });
 
 */
+
+/**
+ * Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+// My solution:
+function vowels(str) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+  let arr = str.split("");
+
+  vowels.forEach((v) => {
+    if (arr.includes(v)) {
+      arr = arr.filter((e) => e !== v);
+    }
+  });
+
+  return arr.join("");
+}
+
+const str = "This website is for losers LOL!";
+console.log(vowels(str));
+
+// other users:
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+ * 
+ */
