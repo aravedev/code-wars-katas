b743ce90b6d0206b616f9a2b0212f0dba2b9cae7
aravedev <dravedev@gmail.com>
2022-07-11 10:00:30 +0200

    my first algorithm, trying to find number of blocks

--- test/script.js
+++ test/script.js
@@ -1635,3 +1635,101 @@ function SeriesSum(n) {
 console.log(SeriesSum(15));
 
 */
+
+/*
+Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+
+Examples:
+findNb(1071225) --> 45
+
+findNb(91716553919377) --> -1 
+
+// My solution:
+function findNb(m) {
+  let blocks = 0;
+  let thereIsBlocks = true;
+
+  increseNumber(m);
+
+  function increseNumber(newM) {
+    if (newM < 0) {
+      console.log("m is less than zero, the value of m is: " + newM);
+      thereIsBlocks = false;
+    } else {
+      let nextNumber = newM - (blocks + 1) ** 3;
+      blocks++;
+      console.log("next number is: " + nextNumber);
+      console.log("blocks is: " + blocks);
+
+      if (nextNumber === 0) {
+        return thereIsBlocks;
+      } else {
+        return increseNumber(nextNumber);
+      }
+    }
+  }
+
+  return thereIsBlocks ? blocks : -1;
+}
+
+console.log(findNb(1071225));
+
+/*
+
+refactorized
+
+   let blocks = 0;
+  let thereIsBlocks = true;
+
+  increseNumber(m);
+
+  function increseNumber(newM) {
+    if (newM < 0) {
+      console.log("m is less than zero, the value of m is: " + newM);
+      thereIsBlocks = false;
+    } else {
+      let nextNumber = newM - (blocks + 1) ** 3;
+      blocks++;
+     // console.log("next number is: " + nextNumber);
+     // console.log("blocks is: " + blocks);
+  
+      return nextNumber === 0 ? thereIsBlocks : increseNumber(nextNumber);
+      
+    }
+  }
+
+  return thereIsBlocks ? blocks : -1;
+
+  // testing:
+  describe("Basic tests",function() {
+
+Test.assertEquals(findNb(4183059834009), 2022)
+Test.assertEquals(findNb(24723578342962), -1)
+Test.assertEquals(findNb(135440716410000), 4824)
+Test.assertEquals(findNb(40539911473216), 3568)
+})
+
+// This was the solution of rickansen #devs
+
+function findNb(m) {
+
+  let counter = 0
+  let arr = []
+  let sum = arr.reduce((a,b) => a + b**3, 0)
+
+    for (let i = 1; sum < m; i++){
+      if (sum < m){
+        arr.push(i)
+        sum = arr.reduce((a,b) => a + b**3, 0)
+        counter++
+      }
+    }
+
+  return sum === m ? counter : -1
+}
+
+*/
