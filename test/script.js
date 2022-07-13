17b32eb9efcc33e64ec3472c383961b9b5f43cea
aravedev <dravedev@gmail.com>
2022-07-13 13:17:54 +0200

    my first difficult kata

--- test/script.js
+++ test/script.js
@@ -1862,3 +1862,45 @@ let s = "bitcoin take over the world maybe who knows perhaps";
 console.log(findShort(s));
 
 */
+
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))((" 
+Notes
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+My solution
+function duplicateEncode(word) {
+  let str = word.toLowerCase().split("");
+  let filterChars = [];
+  let charsDuplicates = [];
+  let charsNoDuplicates = [];
+
+  str.forEach((charDuplicated, index) => {
+    filterChars = str.filter((e) => e == charDuplicated);
+
+    return filterChars.length > 1
+      ? charsDuplicates.push(index)
+      : charsNoDuplicates.push(index);
+  });
+
+  charsNoDuplicates.forEach((noDplicated) => {
+    str[noDplicated] = "(";
+  });
+
+  charsDuplicates.forEach((Dplicated) => {
+    str[Dplicated] = ")";
+  });
+
+  return str.join("");
+}
+
+let s = "recede";
+console.log(duplicateEncode(s));
+
+*/
