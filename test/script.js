7cd714dad1a095ce705ac993b68a6d7c122bf252
aravedev <dravedev@gmail.com>
2022-07-23 09:11:25 +0200

    Solving tribonacci using recursion method, Im proud of my self, I should do more problems like that

--- test/script.js
+++ test/script.js
@@ -2431,3 +2431,97 @@ Test.assertEquals(century(89), 1, 'Testing for year 89');
   });
 });
 */
+
+/* Make a review of this solution:
+function deleteNth(arr, x) {
+  var obj = {};
+  return arr.filter(function (number) {
+    obj[number] = obj[number] ? obj[number] + 1 : 1;
+    return obj[number] <= x;
+  });
+}
+
+console.log(deleteNth([1, 1, 6, 2, 7, 4, 9, 7, 7, 3, 6, 5, 1, 7], 3));
+
+function test(arr, x) {
+  let obj = {};
+  console.log(obj);
+
+  arr.filter((number) => {
+    obj[number] = obj[number] ? obj[number] + 1 : 1;
+    console.log(obj);
+  });
+}
+
+console.log(test([1, 1, 6, 2, 7, 4, 9, 7, 7, 3, 6, 5, 1, 7], 3));
+
+const objeto = {
+  1: "primero",
+  2: "segundo",
+};
+
+console.log(objeto[1]);
+
+*/
+
+/*
+
+Well met with Fibonacci bigger brother, AKA Tribonacci.
+
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+If you enjoyed this kata more advanced and generalized version of it can be found in the Xbonacci kata
+
+[Personal thanks to Professor Jim Fowler on Coursera for his awesome classes that I really recommend to any math enthusiast and for showing me this mathematical curiosity too with his usual contagious passion :)]
+
+
+My solution: Using recursion method
+
+function tribonacci(signature, n, a = 0) {
+  if (n === 0) {
+    return [];
+  } else if (n > 3) {
+    console.log(`signature is ${signature}`);
+    let sum = signature[a] + signature[a + 1] + signature[a + 2];
+    signature.push(sum);
+    console.log(a, `count: ${signature.length}`);
+
+    // n will be decreasing -1 and a+1 each time, if n===3 then skips until the last else and returns signature.
+    return tribonacci(signature, n - 1, a + 1);
+  } else if (n === 2) {
+    return [signature[0], signature[1]];
+  } else if (n === 1) {
+    return [signature[0]];
+  } else {
+    return signature;
+  }
+}
+
+let arr = [1, 1, 1];
+console.log(tribonacci(arr, 1));
+
+// testing:
+describe("Basic tests",function(){
+Test.assertSimilar(tribonacci([1,1,1],10),[1,1,1,3,5,9,17,31,57,105])
+Test.assertSimilar(tribonacci([0,0,1],10),[0,0,1,1,2,4,7,13,24,44])
+Test.assertSimilar(tribonacci([0,1,1],10),[0,1,1,2,4,7,13,24,44,81])
+Test.assertSimilar(tribonacci([1,0,0],10),[1,0,0,1,1,2,4,7,13,24])
+Test.assertSimilar(tribonacci([0,0,0],10),[0,0,0,0,0,0,0,0,0,0])
+Test.assertSimilar(tribonacci([1,2,3],10),[1,2,3,6,11,20,37,68,125,230])
+Test.assertSimilar(tribonacci([3,2,1],10),[3,2,1,6,9,16,31,56,103,190])
+Test.assertSimilar(tribonacci([1,1,1],1),[1])
+Test.assertSimilar(tribonacci([300,200,100],0),[])
+Test.assertSimilar(tribonacci([0.5,0.5,0.5],30),[0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5])
+})
+
+*/
