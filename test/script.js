12f93e8245e23781096d77b363c79cdd5d576fbb
aravedev <dravedev@gmail.com>
2022-07-27 09:21:28 +0200

    mexican wave, fun kata

--- test/script.js
+++ test/script.js
@@ -2875,3 +2875,69 @@ function rot13(message) {
 
 }
 */
+
+/*
+
+Nathan loves cycling.
+
+Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+For example:
+
+time = 3 ----> litres = 1
+
+time = 6.7---> litres = 3
+
+time = 11.8--> litres = 5
+
+// My solution:
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+console.log(litres(6.7));
+
+*/
+
+/*
+
+The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position.
+
+The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+//My solution:
+
+function wave(str) {
+  console.log(`input: ${str}`);
+  if (str.length > 0) {
+    let arr = str.split("");
+    let result = [];
+
+    let num = 0;
+    arr.forEach((e, index) => {
+      if (e !== " ") {
+        arr[index] = e.toUpperCase();
+        result.push(arr.join(""));
+        arr[index] = e.toLowerCase();
+      }
+    });
+
+    return result;
+  } else {
+    return [];
+  }
+}
+
+const str1 = "two worlds";
+console.log(wave(" gap "));
+*/
