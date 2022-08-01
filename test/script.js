6674c89ae2a012674f9111dbf25542e21301ea71
aravedev <dravedev@gmail.com>
2022-08-01 13:42:03 +0200

    functional programming challenge 5kyu , try to solve it later again

--- test/script.js
+++ test/script.js
@@ -3148,3 +3148,78 @@ function persistence(num, count = 0) {
 console.log(persistence(39));
 
 */
+
+/*
+Functional programming: 
+
+
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())); // must return 35
+four(plus(nine())); // must return 13
+eight(minus(three())); // must return 5
+six(dividedBy(two())); // must return 3
+Requirements:
+
+There must be a function for each number from 0 ("zero") to 9 ("nine")
+There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+Each calculation consist of exactly one operation and two numbers
+The most outer function represents the left operand, the most inner function represents the right operand
+Division should be integer division. For example, this should return 2, not 2.666666...:
+eight(dividedBy(three()));
+
+// Solution from forum, this time I couldnt solve it. I spent 4 hours trying to figure it out
+
+function zero(fn) {
+  return !fn ? 0 : fn(0);
+}
+function one(fn) {
+  return !fn ? 1 : fn(1);
+}
+function two(fn) {
+  return !fn ? 2: fn(2);
+}
+function three(fn) {
+  return !fn ? 3: fn(3);
+}
+function four(fn) {
+  return !fn ? 4: fn(4);
+}
+function five(fn) {
+  return !fn ? 5 : fn(5);
+}
+function six(fn) {
+  return !fn ? 6 : fn(6);
+}
+function seven(fn) {
+  return !fn ? 7 : fn(7);
+}
+function eight(fn) {
+  return !fn ? 8 : fn(8);
+}
+function nine(fn) {
+  return !fn ? 9 : fn(9);
+}
+
+function plus(x) {
+  return function(y){
+    return y + x;
+  }
+}
+function minus(x) {
+  return function(y){
+    return y - x;
+  }
+}
+function times(x) {
+  return function(y){
+    return y * x;    
+  }
+}
+function dividedBy(x) {
+  return function (y){
+    return Math.trunc(y / x);
+  }
+}
+
+*/
