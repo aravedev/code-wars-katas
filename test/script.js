805978214568592f9d2f1ff65efdbee3d5bd5b83
aravedev <dravedev@gmail.com>
2022-08-09 11:45:36 +0200

    solved 3 katas 8 kyu today

--- test/script.js
+++ test/script.js
@@ -4046,3 +4046,86 @@ console.log(incrementString(str));
 
 
 */
+
+/*
+
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+//My solution:
+
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+      break;
+    case 1:
+      return "One";
+      break;
+    case 2:
+      return "Two";
+      break;
+    case 3:
+      return "Three";
+      break;
+    case 4:
+      return "Four";
+      break;
+    case 5:
+      return "Five";
+      break;
+    case 6:
+      return "Six";
+      break;
+    case 7:
+      return "Seven";
+      break;
+    case 8:
+      return "Eight";
+      break;
+
+    default:
+      return "Nine";
+      break;
+  }
+}
+
+*/
+
+/*
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+
+//My solution:
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) {
+    return 100;
+  } else if (exam > 75 && projects > 4) {
+    return 90;
+  } else if (exam > 50 && projects > 1) {
+    return 75;
+  } else {
+    return 0;
+  }
+}
+console.log(finalGrade(55, 0));
+
+*/
+
+//100, 12 --> 100
+//99, 0 --> 100
+//10, 15 --> 100
