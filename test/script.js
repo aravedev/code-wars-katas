3148b02ed0f879540ccd64409db9317d68db06e8
aravedev <dravedev@gmail.com>
2022-08-10 09:45:44 +0200

    4 katas solved easy questions

--- test/script.js
+++ test/script.js
@@ -4167,3 +4167,71 @@ function solution(string) {
 }
 
 */
+
+/*
+
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// My solution:
+function longest(s1, s2) {
+  let str = s1.concat(s2);
+  str = new Set(str);
+  str = [...str].sort().join("");
+  return str;
+}
+
+const a = "xyaabbbccccdefww";
+const b = "xxxxyyyyabklmopq";
+
+console.log(longest(a, b));
+
+*/
+
+/*
+ return the number from the string
+
+ // my solution:
+ function getAge(inputString) {
+  let str = inputString.split("");
+  let age = +str[0];
+  return age;
+}
+
+console.log(getAge("4 years old"));
+
+*/
+
+/*
+
+Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+// My solution:
+function minMax(arr) {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+
+  return max !== min ? [min, max] : [max, max];
+}
+
+console.log(minMax([1]));
+
+*/
