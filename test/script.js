bfc7900932000d8f718972fe2450faf5c1150c7c
aravedev <dravedev@gmail.com>
2022-08-25 08:51:05 +0200

    2 katas 7 and 8 kyu each one

--- test/script.js
+++ test/script.js
@@ -4902,3 +4902,44 @@ function isPalindrome(x) {
 
 console.log(isPalindrome("Abba"));
 */
+
+/*
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+// My solution:
+function capitals(word) {
+  let arr = [];
+  word.split("").forEach((e, index) => {
+    if (e === e.toUpperCase()) {
+      arr.push(index);
+    }
+  });
+  return arr;
+}
+
+const str = "CodEWaRs";
+console.log(capitals(str));
+*/
+
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+
+// My solution:
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((e) => e + e)
+    .join("");
+}
+
+const str = "String";
+console.log(doubleChar(str));
+*/
