58111ee162b337028aa7aa2c12d58aa35046cf28
aravedev <dravedev@gmail.com>
2022-08-29 12:07:13 +0200

    1 kata 7 kyu and 1 kata 5 kyu, it was an algorithm,something difficult , I couldnt solve it

--- test/script.js
+++ test/script.js
@@ -4993,3 +4993,59 @@ const sequenceSum = (begin, end, step) => {
 
 console.log(sequenceSum(1, 5, 1));
 */
+
+/*
+
+The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// should be 6: [4, -1, 2, 1]
+Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+// Forum answer:
+function maxSequence(arr) {
+  let min = 0;
+  let ans = 0;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+
+  return ans;
+}
+
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSequence(arr));
+
+
+*/
+
+/*
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+// My solution.
+function gimme(triplet) {
+  let copy = [...triplet].sort((a, b) => a - b);
+  return triplet.indexOf(copy[1]);
+}
+
+const arr = [2, 3, 1];
+console.log(gimme(arr));
+*/
