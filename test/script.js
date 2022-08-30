90fc2deffa2ab865c44c9c3c7b18df8d4eae5042
aravedev <dravedev@gmail.com>
2022-08-30 11:04:48 +0200

    1kata 5kyu and 1kata 7kyu

--- test/script.js
+++ test/script.js
@@ -5049,3 +5049,78 @@ function gimme(triplet) {
 const arr = [2, 3, 1];
 console.log(gimme(arr));
 */
+
+/*
+
+How I crossed a mountainous desert the smart way.
+The directions given to the man are, for example, the following (depending on the language):
+
+["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"].
+or
+{ "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+or
+[North, South, South, East, West, North, West]
+You can immediately see that going "NORTH" and immediately "SOUTH" is not reasonable, better stay to the same place! So the task is to give to the man a simplified version of the plan. A better plan in this case is simply:
+
+["WEST"]
+or
+{ "WEST" }
+or
+[West]
+Other examples:
+In ["NORTH", "SOUTH", "EAST", "WEST"], the direction "NORTH" + "SOUTH" is going north and coming back right away.
+
+The path becomes ["EAST", "WEST"], now "EAST" and "WEST" annihilate each other, therefore, the final result is [] (nil in Clojure).
+
+In ["NORTH", "EAST", "WEST", "SOUTH", "WEST", "WEST"], "NORTH" and "SOUTH" are not directly opposite but they become directly opposite after the reduction of "EAST" and "WEST" so the whole path is reducible to ["WEST", "WEST"].
+
+// My solution:
+
+function dirReduc(arr) {
+  let copy = [...arr];
+
+  for (let i = copy.length; i >= 0; i--) {
+    if (
+      (copy[i] == "NORTH" && copy[i - 1] == "SOUTH") ||
+      (copy[i] == "SOUTH" && copy[i - 1] == "NORTH")
+    ) {
+      copy.splice(i - 1, 2);
+    } else if (
+      (copy[i] == "EAST" && copy[i - 1] == "WEST") ||
+      (copy[i] == "WEST" && copy[i - 1] == "EAST")
+    ) {
+      copy.splice(i - 1, 2);
+    }
+  }
+
+  return copy;
+}
+
+const str = ["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"];
+console.log(dirReduc(str));
+
+
+*/
+
+/*
+
+Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+// My solution:
+
+String.prototype.isUpperCase = function () {
+  return this == this.toUpperCase();
+};
+
+console.log("hELLO I AM DONALD".isUpperCase());
+*/
