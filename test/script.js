9179aacb17dd24aef5b84781893c2fe25d9297ff
aravedev <dravedev@gmail.com>
2022-09-01 12:11:17 +0200

    1kata 5kyu about fibonacci product, it was so so

--- test/script.js
+++ test/script.js
@@ -5180,3 +5180,100 @@ console.log(reverseLetter(str));
 
 
 */
+
+/*
+
+  An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+
+// My solution:
+function isAnagram(test, original) {
+  let word1 = test.toLowerCase().split("").sort().join("");
+  let word2 = original.toLowerCase().split("").sort().join("");
+
+  return word1 === word2 ? true : false;
+}
+
+console.log(isAnagram("Buckethead", "DeathCubeK"));
+
+*/
+/*
+function fibo(position) {
+  let x = 1.618034 ** position - (1 - 1.618034) ** position;
+  x = x / Math.sqrt(5);
+
+  return Math.round(x);
+}
+
+console.log(fibo(9));
+*/
+
+/*
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+
+
+
+//My solution:
+  function productFib(prod, n = 1) {
+  let x = fibo(n);
+  let y = fibo(n + 1);
+  let result = x * y;
+
+  if (result === prod) {
+    console.log(result);
+    return [x, y, true];
+  } else if (prod < result) {
+    console.log(result - prod);
+    return [x, y, false];
+  } else {
+    return productFib(prod, n + 1);
+  }
+}
+
+function fibo(position) {
+  let x = 1.61803398875 ** position - (1 - 1.61803398875) ** position;
+  x = x / Math.sqrt(5);
+
+  return Math.round(x);
+}
+
+console.log(productFib(714));
+
+//Forum solution:
+function productFib(prod){
+  var n = 0;
+  var nPlus = 1;  
+  while(n*nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n*nPlus===prod];
+}
+
+*/
