dfc9ffc14d312cad84411336b1cbf1be58bf5f81
aravedev <dravedev@gmail.com>
2022-09-07 11:12:05 +0200

    5kyu kata find the domain of website

--- test/script.js
+++ test/script.js
@@ -5407,3 +5407,53 @@ function between(a, b) {
 
 console.log(between(1, 4));
 */
+/*
+function domainName(url) {
+  let str = url;
+  let com = str.indexOf(".com");
+  let result = "";
+  str = str.substr(0, com);
+
+  let dash = str.indexOf("//");
+  let www = str.indexOf("www.");
+
+  if (dash !== -1) {
+    result = str.substr(dash + 2);
+    return result;
+  } else if (www !== -1) {
+  }
+
+  console.log(str);
+  console.log(dash);
+  console.log(www);
+  console.log(result);
+}
+*/
+
+/*
+
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+// My solution with help from the forum
+
+function domainName(url) {
+  
+  
+  url = url
+
+    .replace("https://", " ")
+    .replace("http://", "")
+    .replace("www.", "")
+    .split(".");
+
+  return url[0];
+}
+
+const str = "http://google.co.jp";
+console.log(domainName(str));
+
+*/
